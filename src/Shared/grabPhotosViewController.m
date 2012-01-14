276996e542189f369643b4207dfc69bcd4e05a37
Ethan Sherbondy <sherbondye@gmail.com>
2012-01-14 18:54:02 -0500

    adjusts rows count based on orientation.

--- src/Shared/grabPhotosViewController.m
+++ src/Shared/grabPhotosViewController.m
@@ -37,6 +37,8 @@
 		swypBackedPhotoDataSource *	photoDatasource	= [[swypBackedPhotoDataSource alloc] initWithBackingDelegate:self];	
 		//make sure to set the data-source! If there are bugs, you should submit a pull-request!
 		[[[self swypWorkspace] contentManager] setContentDataSource:photoDatasource];
+        
+        SRELS(photoDatasource);
 		
 		//we create our favorite content display controller
 		//we'll be adding data later see (elcImagePickerController:didFinishPickingMediaWithInfo:)
